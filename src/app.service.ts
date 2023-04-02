f73a7baae2bcf765383f267adac297525e15fbba
mthang1801 <mthang1801@gmail.com>
2023-04-02 09:51:47 +0700

    add user

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+	const a = 5 ; 
+	let b = a ;
+	if (b === a) return 'Hello World!';
+	throw new Error("Something went wrong!")
   }
 }
